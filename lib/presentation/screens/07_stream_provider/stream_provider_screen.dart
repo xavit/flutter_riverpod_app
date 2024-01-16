c46da3c2372ffc57a61283cffe26722958e6d28b
Javier Claros <javier.claros@gestora.bo>
2024-01-16 10:34:04 -0400

    Stream Provider

--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/screens/providers/providers.dart';
 
 class StreamProviderScreen extends StatelessWidget {
   const StreamProviderScreen({super.key});
@@ -10,6 +11,45 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: const StreamView(),
+    );
+  }
+}
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userInChat = ref.watch(usersInChatsProvider);
+
+    if (userInChat.isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (userInChat.hasError) {
+      return SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Center(
+          child: Text(userInChat.error.toString()),
+        ),
+      );
+    }
+
+    final users = userInChat.value!;
+
+    return ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        final user = users[index];
+
+        return ListTile(
+          title: Text(user),
+        );
+      },
     );
   }
 }
---
+++ lib/presentation/screens/providers/stream_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'stream_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$usersInChatsHash() => r'0d2f6cf93b80569e70eb4ffbfd940a7afe97067a';
+
+/// See also [usersInChats].
+@ProviderFor(usersInChats)
+final usersInChatsProvider = StreamProvider<List<String>>.internal(
+  usersInChats,
+  name: r'usersInChatsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$usersInChatsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef UsersInChatsRef = StreamProviderRef<List<String>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
